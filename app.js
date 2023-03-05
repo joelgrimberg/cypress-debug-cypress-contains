fe510513e5817c7820a838d3502753169063fb9b
Joël <joel@joelgrimberg.nl>
2023-03-05 08:39:56 +0100

    iterate

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
-const express = require('express');
-const frontend = express();
-const backend = express();
-const frontendPort = 3000;
-const backendPort = 3001;
+const express = require('express')
 
-frontend.get('/', (req, res) => {
-  res.send('Hi user !');
-});
+const frontend = express()
+const frontendPort = 3000
+
+const backend = express()
+const backendPort = 3001
+
+async function fetchDataJSON() {
+  const response = await fetch('http://localhost:3001/', { method: 'GET' })
+  const user = await response.json()
+  return user
+}
+
+frontend.get('/', (_req, res) => {
+  fetchDataJSON().then((user) => {
+    res.send(`Hi ${user.username}`)
+  })
+})
 
 backend.get('/', (req, res) => {
-  res.json({ user: 'Joël' });
-});
+  res.json({ username: 'J.' })
+})
 
 frontend.listen(frontendPort, () => {
-  console.log(`Debugging cy.contains on frontendPort ${frontendPort}`);
-});
+  console.log(`Debugging cy.contains on frontendPort ${frontendPort}`)
+})
 
 backend.listen(backendPort, () => {
-  console.log(`Debugging cy.contains on backendPort ${backendPort}`);
-});
+  console.log(`Debugging cy.contains on backendPort ${backendPort}`)
+})
